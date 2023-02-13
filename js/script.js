0bfa5b2a746898453aed9fc5685b3d5c5e3dc174
gayeongogo <rkdus1357@gmail.com>
2023-02-14 00:47:43 +0900

    add fullpage

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//풀페이지
+
+$(function () {
+    $('#fullpage').fullpage({
+        //options here
+        anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
+        navigation: true,
+    });
+});

358207b7138b4104b861460451f787567b6ea196
gayeongogo <rkdus1357@gmail.com>
2023-02-14 18:17:16 +0900

    add typed.js file

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ $(function () {
         navigation: true,
     });
 });
+
+//타이핑
+
+    $('.home-main-text').typed({
+        strings:["안녕하세요"], //[]안 반점 기준 지워지고 다시 타이핑
+        typeSpeed:300,
+        backDelay:1000,
+        loop: true,
+        cursorChar:" "
+    });
